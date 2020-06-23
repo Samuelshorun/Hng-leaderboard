22b20fa61ceda197a1ea04b602ab5d805a915fc7
shorun <shorun@users.noreply.github.com>
2020-06-23 02:56:15 +0100

    update2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,130 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Leadership Board</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/leader.css">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/fontawesome/css/brands.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+</head>
 
-            .position-ref {
-                position: relative;
-            }
+<body>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header"><a href="#">
+                <span><img src="img/HNG.png" alt="HNG-LOGO"><h3>HNG INTERNSHIP</h3></span>
+                <strong>HNG</strong></a>
+            </div>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    HNG-Leaderboard
-                </div>
-
+            <ul class="list-unstyled components mb-5">
                 
+                <li>
+                    <a href="#">
+                        <i class="fas fa-home"></i>
+                        DashBoard
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-user"></i>
+                        Interns
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="#">
+                        <i class="fas fa-align-left"></i>
+                        Leaderboard
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-paper-plane"></i>
+                        Mentors
+                    </a>
+                </li>
+            </ul>
+            <div class="signup d-flex flex-column mt-5">
+                <h4 class="text-align-center mx-auto"><a class="btn btn-dark" href="#">Sign out</a></h4><br>
+
+                <p class="copy mx-auto">&copy HNG INTERNSHIP 2020</p>
+
+
+            </div>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-dark">
+                        <i class="fas fa-align-left"></i>
+                        <span></span>
+                    </button>
+            </nav>
+                
+    <div class="container">
+                    <!-------------------------------------------Table------------------------------------------------------->
+
+        <div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>SLACK NAME</th>
+                        <th>E-MAIL ADRESS</th>
+                        <th>POINT</th>
+                        <th class="pl-1 pl-lg-5">SHARE</th>
+                    </tr>
+                </thead>
+                    <tbody id="tbody">
+                      
+                    </tbody>
+      </table>
+
+
+                    <!---------------------------------------------TABLE END------------------------------------------------>
+
+
+
+                </div>
+            <div class="d-flex justify-content-between flex-row text-center">
+                <input type="button" class="btn btn-primary" id="first" onclick="firstPage()" value="first" />
+                <input type="button" class="btn btn-primary" id="next" onclick="nextPage()" value="next" />
+                <input type="button" class="btn btn-primary" id="previous" onclick="previousPage()" value="previous" />
+                <input type="button" class="btn btn-primary" id="last" onclick="lastPage()" value="last" />
             </div>
         </div>
-    </body>
+    </div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+   <script src="js/jquery-3.5.1.min.js"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/dash.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
 </html>
